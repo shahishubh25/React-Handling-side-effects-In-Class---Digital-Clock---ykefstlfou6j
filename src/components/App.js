3d2a218e54839246cbb924aae7ecd43e7e0c7aff
Shubham Shahi <shubhamshahi552@gmail.com>
2022-02-24 18:50:30 +0530

    gg

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
 
+const App = () => {
+  const [date, setDate] = useState("");
+  useEffect(() => {
+    let time = new Date().toLocaleString();
+    console.log(time);
+    setDate(time);
+    let tim = setInterval(() => {
+      let clock = new Date().toLocaleString();
+      setDate(clock);
+    }, 1000);
+
+    return () => {
+      clearInterval(tim);
+    };
+  }, []);
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">{date}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
